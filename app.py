86243efc0493186fec89d8c31de03913e51dcc47
Marc Chametzky <marc@bluevine.net>
2025-06-05 20:28:00 -0400

    Rewrite game using Flask

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+import random
+
+app = Flask(__name__)
+app.secret_key = 'dev-secret-key'
+
+TOTAL_NUMBERS = 9
+
+
+def start_game():
+    session['numbers'] = random.sample(range(1, TOTAL_NUMBERS + 1), TOTAL_NUMBERS)
+    session['index'] = 1
+    session['clicked'] = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if 'numbers' not in session:
+        start_game()
+
+    message = ''
+
+    if request.method == 'POST':
+        num = int(request.form.get('num', 0))
+        expected = session.get('index', 1)
+        if num == expected:
+            session['clicked'].append(num)
+            session['index'] = expected + 1
+            if expected == TOTAL_NUMBERS:
+                message = 'Level complete!'
+        else:
+            message = 'Wrong button. Try again!'
+
+    return render_template(
+        'index.html',
+        numbers=session['numbers'],
+        clicked=session.get('clicked', []),
+        message=message,
+    )
+
+
+@app.route('/restart')
+def restart():
+    start_game()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
